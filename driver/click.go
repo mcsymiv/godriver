76688b0ac8b40a9ea7c0e6a28577e20a242a24f9
mcsymiv <s.mcsymiv@gmail.com>
2024-03-13 10:38:30 +0200

    upd click strategy

--- driver/click.go
+++ driver/click.go
@@ -1,18 +1,17 @@
 package driver
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 type clickStrategy struct {
-	http.Client
+	dr *Driver
 }
 
 func (cl clickStrategy) Execute(req *http.Request) (*http.Response, error) {
 	log.Printf("click on: %s", req.URL.Path)
-	return cl.Client.Do(req)
+	return cl.dr.Client.HTTPClient.Do(req)
 }
 
 func (el *Element) Click() *Element {
@@ -22,29 +21,9 @@ func (el *Element) Click() *Element {
 		Method:         http.MethodPost,
 		Data:           marshalData(&Empty{}),
 		Strategies: []CommandExecutor{
-			&clickStrategy{},
-		},
-	})
-
-	return el
-}
-
-func (el *Element) ClickV2() *Element {
-
-	el.Driver.Script(
-		fmt.Sprintf(
-			`document.querySelector("%s").addEventListener("click",function(e){return "any value"})`,
-			el.Selector.Value,
-		),
-	)
-
-	el.Client.ExecuteCmd(&Command{
-		Path:           "/element/%s/click",
-		PathFormatArgs: []any{el.Id},
-		Method:         http.MethodPost,
-		Data:           marshalData(&Empty{}),
-		Strategies: []CommandExecutor{
-			&clickStrategy{},
+			&clickStrategy{
+				dr: el.Driver,
+			},
 		},
 	})
 
