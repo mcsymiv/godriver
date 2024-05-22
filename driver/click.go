3f034d9905c471310190121a6714cee36e54e808
mcsymiv <mcs@mcsymivs-MacBook-Pro.local>
2024-05-22 10:43:00 +0300

    rm steps

--- driver/click.go
+++ driver/click.go
@@ -1,10 +1,31 @@
 package driver
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/mcsymiv/godriver/by"
 )
 
 func (el *Element) Click() *Element {
+
+	el.Client.ExecuteCommand(&Command{
+		Path:           PathElementClick,
+		PathFormatArgs: []any{el.Id},
+		Method:         http.MethodPost,
+		Data:           marshalData(&Empty{}),
+	}, &Empty{})
+
+	return el
+}
+
+func (d *Driver) Cl(selector string) *Element {
+	w3cBy := by.Strategy(selector)
+
+	el, err := f(w3cBy, d)
+	if err != nil {
+		panic(fmt.Errorf("unable to find element, got: %v\n", err))
+	}
 
 	el.Client.ExecuteCommand(&Command{
 		Path:           PathElementClick,
